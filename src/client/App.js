74171498009e000b3ff3fd7b80420b5fba12807a
maxpluez <max@pop-os.localdomain>
2020-07-10 13:55:56 -0700

    Work in progress...

--- src/client/App.js
+++ src/client/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import axios from 'axios';
-import '@instructure/canvas-theme';
+// before mounting your React application:
+import theme from '@instructure/canvas-high-contrast-theme';
 import './app.css';
 
 import { View } from '@instructure/ui-view';
 import { Text } from '@instructure/ui-text';
+import { Button, CondensedButton } from '@instructure/ui-buttons';
+import { Heading } from '@instructure/ui-heading';
+import { NumberInput } from '@instructure/ui-number-input';
+import { Billboard } from '@instructure/ui-billboard';
+import { IconUserLine, IconCheckLine } from '@instructure/ui-icons';
+import { Table } from '@instructure/ui-table';
+
+theme.use({ overrides: { colors: { brand: 'red' } } });
 
 const App = () => {
-  const [members, setMembers] = React.useState([]);
-  // [chosenMember, chooseMember] = React.useState({ name: '' });
-
   const getLtikPromise = new Promise((resolve, reject) => {
     const searchParams = new URLSearchParams(window.location.search);
     let potentialLtik = searchParams.get('ltik');
     if (!potentialLtik) {
-      potentialLtik = sessionStorage.getItem('ltik'); // eslint-disable-line no-undef
+      potentialLtik = sessionStorage.getItem('ltik');
       if (!potentialLtik) reject(new Error('Missing lti key.'));
     }
     resolve(potentialLtik);
@@ -29,19 +36,74 @@ const App = () => {
     });
   });
 
+  const [members, setMembers] = React.useState([]);
+
   const retrieveMembers = () => {
-    setLtikPromise.then((res) => {
-      axios.get(`/api/members?ltik=${res}`).then((list) => {
-        console.log(`res from server: ${list.data}`);
-        setMembers(res.data);
+    setLtikPromise.then((ltik) => {
+      axios.get(`/api/members?ltik=${ltik}`).then((list) => {
+        setMembers(list.data);
       }).catch((err) => {
-        console.log('error retrieving from server');
         console.log(err);
       });
     }).catch((err) => {
       console.log(err);
     });
   };
+
+  React.useEffect(retrieveMembers, []);
+
+  const [usersShown, showUsers] = React.useState(false);
+  const [gradesShown, showGrades] = React.useState(false);
+  if (!usersShown && !gradesShown) {
+    return (
+      <View
+        display="flex"
+        background="brand"
+        margin="0"
+        padding="xx-large"
+        textAlign="center"
+      >
+        <Billboard
+          margin="auto"
+          message="User Center"
+          onClick={() => {
+            showUsers(true);
+          }}
+          hero={size => <IconUserLine size={size} />}
+
+        />
+        <Billboard
+          margin="auto"
+          message="Grading Center"
+          onClick={() => {
+            showGrades(true);
+          }}
+          hero={size => <IconCheckLine size={size} />}
+        />
+      </View>
+    );
+  }
+  if (usersShown) {
+    return (
+      <UserCenter
+        members={members}
+        retrieveMembers={retrieveMembers}
+        showUsers={showUsers}
+        setLtikPromise={setLtikPromise}
+      />
+    );
+  }
+  return (
+    <GradingCenter
+      showGrades={showGrades}
+      setLtikPromise={setLtikPromise}
+      members={members}
+    />
+  );
+};
+
+const UserCenter = ({ members, retrieveMembers, showUsers }) => {
+  const [chosenMember, chooseMember] = React.useState({});
 
   React.useEffect(retrieveMembers, []);
 
@@ -54,15 +116,33 @@ const App = () => {
     >
       <UserList
         members={members}
+        chooseMember={chooseMember}
+        showUsers={showUsers}
       />
-      <UserPanel />
+      <UserPanel
+        chosenMember={chosenMember}
+      />
     </View>
   );
 };
 
-// eslint-disable-next-line react/prop-types
-const UserList = ({ members }) => {
-  console.log(`members inside UserList: ${members}`);
+const UserList = ({ members, chooseMember, showUsers }) => {
+  const selectMember = (event) => {
+    chooseMember(members.filter(member => member.name === event.target.innerText)[0]);
+  };
+
+  let membersElement = <View>Getting members...</View>;
+  if (members.length !== 0) {
+    membersElement = members.map(member => (
+      <CondensedButton
+        color="primary-inverse"
+        display="block"
+        onClick={selectMember}
+      >
+        {member.name}
+      </CondensedButton>
+    ));
+  }
 
   return (
     <View
@@ -71,13 +151,39 @@ const UserList = ({ members }) => {
       padding="small"
       width="50%"
     >
-      <Text>User List</Text>
+      <CondensedButton
+        color="primary-inverse"
+        display="block"
+        onClick={() => {
+          showUsers(false);
+        }}
+      >
+        {'< Back'}
+      </CondensedButton>
+      <br />
+      {membersElement}
     </View>
   );
 };
 
-const UserPanel = () => {
-  console.log('');
+const UserPanel = ({ chosenMember }) => {
+  let profileElement = <Text>Choose a member on the left to begin ...</Text>;
+  if (chosenMember.name) {
+    profileElement = (
+      <View>
+        <Heading level="h2">{chosenMember.name}</Heading>
+        <View display="block">
+          {chosenMember.roles.map((role, index) => {
+            if (index === 0) {
+              return (<Text>{role}</Text>);
+            }
+            return (<Text>{`, ${role}`}</Text>);
+          })}
+        </View>
+        <Text display="block">{chosenMember.email}</Text>
+      </View>
+    );
+  }
   return (
     <View
       as="div"
@@ -86,7 +192,93 @@ const UserPanel = () => {
       width="50%"
       shadow="topmost"
     >
-      <Text>User Profile</Text>
+      {profileElement}
+    </View>
+  );
+};
+
+const GradingCenter = ({ showGrades, setLtikPromise, members }) => {
+  const [grades, setGrades] = React.useState([]);
+  const retrieveGrades = () => {
+    setLtikPromise.then((ltik) => {
+      axios.get(`/api/grades?ltik=${ltik}`).then((list) => {
+        setGrades(list.data);
+      });
+    });
+  };
+  React.useEffect(retrieveGrades, []);
+
+  let tableElement = <Text>Getting grades ...</Text>;
+  if (grades.length !== 0) {
+    tableElement = (
+      <Table
+        caption="Top rated movies"
+        hover
+      >
+        <Table.Head>
+          <Table.Row>
+            <Table.ColHeader id="name">Name</Table.ColHeader>
+            <Table.ColHeader id="activity">Activity</Table.ColHeader>
+            <Table.ColHeader id="score">Score</Table.ColHeader>
+          </Table.Row>
+        </Table.Head>
+      </Table>
+    );
+  }
+
+  const [scoreGivenStr, giveScore] = useState('');
+  const handleScoreChange = (event) => {
+    giveScore(event.target.value);
+  };
+
+  const submitGrade = () => {
+    if (!/^[0-9]+\.?[0-9]*$/.test(scoreGivenStr)) {
+      // eslint-disable-next-line no-alert
+      alert('Please enter a proper decimal number!');
+      return;
+    }
+    const grade = {
+      scoreGiven: parseFloat(scoreGivenStr),
+      activityProgress: 'Completed',
+      gradingProgress: 'FullyGraded'
+    };
+    setLtikPromise.then((ltik) => {
+      axios.post(`/api/grades?ltik=${ltik}`, grade).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+  return (
+    <View
+      display="block"
+      background="brand"
+      padding="xx-large"
+    >
+      <CondensedButton
+        color="primary-inverse"
+        display="block"
+        onClick={() => {
+          showGrades(false);
+        }}
+      >
+        {'< Back'}
+      </CondensedButton>
+      <View margin="xx-large">
+        {tableElement}
+      </View>
+      <NumberInput
+        renderLabel={<Text color="primary-inverse">Submit Grade: </Text>}
+        onChange={handleScoreChange}
+      />
+      <Button
+        color="primary"
+        margin="medium"
+        onClick={submitGrade}
+      >
+        Submit
+      </Button>
     </View>
   );
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,37 @@
 // Requiring LTIJS provider
 const Lti = require('ltijs').Provider;
 // const cors = require('cors');
+require('dotenv').config();
 
 // Creating a provider instance
 const lti = new Lti(
-  'SAMPLEKEY',
+  process.env.LTI_KEY,
   // Setting up database configurations
-  { url: 'mongodb://localhost/ltimoodle' }
+  {
+    url: `mongodb://${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+    connection: { user: process.env.DB_USER, pass: process.env.DB_PASS }
+  },
+  { appUrl: '/', loginUrl: '/login', logger: true }
 );
 
 async function setup() {
   // Deploying provider, connecting to the database and starting express server.
   await lti.deploy();
   // Register Moodle as a platform
-  await lti.registerPlatform({
-    url: 'http://localhost:8000',
+  const plat = await lti.registerPlatform({
+    url: process.env.PLATFORM_URL,
     name: 'Local Moodle',
-    clientId: 'o41NEeEz3FUJzXm',
-    authenticationEndpoint: 'http://localhost:8000/mod/lti/auth.php',
-    accesstokenEndpoint: 'http://localhost:8000/mod/lti/token.php',
+    clientId: process.env.PLATFORM_CLIENTID,
+    authenticationEndpoint: process.env.PLATFORM_ENDPOINT,
+    accesstokenEndpoint: process.env.PLATFORM_TOKEN_ENDPOINT,
     authConfig: {
       method: 'JWK_SET',
-      key: 'http://localhost:8000/mod/lti/certs.php'
+      key: process.env.PLATFORM_KEY_ENDPOINT
     }
   });
+
+  // Get the public key generated for that platform
+  console.log(await plat.platformPublicKey());
 
   lti.onConnect((connection, request, response) => {
     lti.redirect(response, 'http://localhost:5000');
@@ -32,15 +40,27 @@ async function setup() {
   // lti.app.use(cors());
 
   // Names and Roles route
-  lti.app.get('/api/members', async (req, res) => {
+  lti.app.get('/api/members', (req, res) => {
+    lti.NamesAndRoles.getMembers(res.locals.token)
+      .then(members => res.send(members.members))
+      .catch(err => res.status(400).send(err));
+  });
+
+  // Grades routes
+  lti.app.get('/api/grades', (req, res) => {
+    lti.Grade.result(res.locals.token).then(grades => res.status(200).send(grades)).catch((err) => {
+      console.log(err);
+      return res.status(400);
+    });
+  });
+
+  lti.app.post('/api/grades', (req, res) => {
     try {
-      console.log(res.locals.token);
-      const result = await lti.NamesAndRoles.getMembers(res.locals.token);
-      console.log(result);
-      return res.send(result);
+      lti.Grade.ScorePublish(res.locals.token, req.body);
+      return res.status(200).send(req.body);
     } catch (err) {
       console.log(err);
-      return res.status(500).send(err);
+      return res.status(400).send(err);
     }
   });
 }
