3c62cf45688f540d73cc5e2f258e40fbdca78f27
Rex Lorenzo <rex@teaching.ucla.edu>
2020-07-13 09:11:34 -0700

    Cleaned server index.js, updated README, fixed .env-dist

--- src/server/index.js
+++ src/server/index.js
@@ -22,6 +22,46 @@ const lti = new Lti(
   options
 );
 
+// When receiving successful LTI launch redirects to app.
+lti.onConnect((token, req, res) => {
+  if (process.env.MODE === 'production') {
+    return res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  }
+  return lti.redirect(res, 'http://localhost:3000');
+});
+
+// Routes.
+
+// Names and Roles route.
+lti.app.get('/api/members', (req, res) => {
+  lti.NamesAndRoles.getMembers(res.locals.token)
+    .then(members => {
+      console.log(members);
+      res.send(members.members);
+    })
+    .catch(err => res.status(400).send(err));
+});
+
+// Grades routes.
+lti.app.get('/api/grades', (req, res) => {
+  lti.Grade.result(res.locals.token)
+    .then(grades => res.status(200).send(grades))
+    .catch(err => {
+      console.log(err);
+      return res.status(400);
+    });
+});
+
+lti.app.post('/api/grades', (req, res) => {
+  try {
+    lti.Grade.ScorePublish(res.locals.token, req.body);
+    return res.status(200).send(req.body);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err);
+  }
+});
+
 async function setup() {
   // Deploying provider, connecting to the database and starting express server.
   await lti.deploy({ port: 8080 });
@@ -42,44 +82,6 @@ async function setup() {
   // Get the public key generated for that platform.
   const plat = await lti.getPlatform(process.env.PLATFORM_URL);
   console.log(await plat.platformPublicKey());
-
-  // When receiving successful LTI launch redirects to app.
-  lti.onConnect((token, req, res) => {
-    if (process.env.MODE === 'production') {
-      return res.sendFile(path.join(__dirname, '../../dist/index.html'));
-    }
-    return lti.redirect(res, 'http://localhost:3000');
-  });
-
-  // Names and Roles route.
-  lti.app.get('/api/members', (req, res) => {
-    lti.NamesAndRoles.getMembers(res.locals.token)
-      .then(members => {
-        console.log(members);
-        res.send(members.members);
-      })
-      .catch(err => res.status(400).send(err));
-  });
-
-  // Grades routes.
-  lti.app.get('/api/grades', (req, res) => {
-    lti.Grade.result(res.locals.token)
-      .then(grades => res.status(200).send(grades))
-      .catch(err => {
-        console.log(err);
-        return res.status(400);
-      });
-  });
-
-  lti.app.post('/api/grades', (req, res) => {
-    try {
-      lti.Grade.ScorePublish(res.locals.token, req.body);
-      return res.status(200).send(req.body);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).send(err);
-    }
-  });
 }
 
 setup();
